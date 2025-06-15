76ff6a95651c294999241c420947090acf64e75f
S.Mohamed Sameer <88232429+sameerthedeveloper@users.noreply.github.com>
2025-06-15 19:51:08 +0530

    Add files via upload

---
+++ pdfList.js
@@ -0,0 +1,25 @@
+window.pdfData = {
+  "MODULE_1": [
+    "Cadmium selenide quantum dots.pdf",
+    "Duralumin.pdf",
+    "Laquier - Constituents and functions.pdf",
+    "Metal alloy.pdf"
+  ],
+  "MODULE_2": [
+    "Synthesis of carbon nanotube by CVD method.pdf",
+    "PN junction solar cells.pdf",
+    "LED.pdf",
+    "ABS polymer - synthesis, properties, applications.pdf",
+    "Nylon 6,6 polymer - synthesis, properties, applications.pdf"
+  ],
+  "MODULE_3": [
+    "Potentiometric MOSFET gas sensor and CO2 gas sensors.pdf",
+    "DNA micro array chips.pdf",
+    "IR touchscreen sensor.pdf"
+  ],
+  "MODULE_4": [
+    "Scanning Electron Microscope.pdf",
+    "Transmission Electron Microscope.pdf",
+    "Two thermal decomposition graph.pdf"
+  ]
+};
